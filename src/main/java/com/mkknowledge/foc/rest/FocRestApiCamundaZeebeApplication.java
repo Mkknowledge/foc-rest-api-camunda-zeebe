7b97f1616c76e4db7d4df58f243102aecb3693d8
Mayur Kandalkar <40908920+Mkknowledge@users.noreply.github.com>
2025-12-08 20:40:51 +0530

    Initial Commit

---
+++ src/main/java/com/mkknowledge/foc/rest/FocRestApiCamundaZeebeApplication.java
@@ -0,0 +1,13 @@
+package com.mkknowledge.foc.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocRestApiCamundaZeebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocRestApiCamundaZeebeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkknowledge/foc/rest/FocRestApiCamundaZeebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkknowledge.foc.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocRestApiCamundaZeebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
